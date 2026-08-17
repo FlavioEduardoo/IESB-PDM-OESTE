b887fc988148acd83017948c8eb8c1e89912fd03
Flávio Eduardo Miranda Correia <flavioeduardo0811@gmail.com>
2026-08-17 19:49:58 -0300

    igor

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { rotulo_btn_cadastro_meta, rotulo_imput_meta, rotulo_lista_metas } from './mensagens';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <TextInput placeholder={rotulo_imput_meta} />
+      <Button title={rotulo_btn_cadastro_meta} />
+      <Text>{rotulo_lista_metas}</Text> 
     </View>
   );
 }
@@ -17,4 +20,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainContainer:{
+    padding: 30,
+  }
 });
---
+++ praticas/pratica03/app-atividade-aula-tarefas/mensagens.js
@@ -0,0 +1,3 @@
+export const rotulo_imput_meta = "Digite sua Meta";
+export const rotulo_btn_cadastro_meta = "Adicionar";
+export const rotulo_lista_metas = "Lista de Metas";
